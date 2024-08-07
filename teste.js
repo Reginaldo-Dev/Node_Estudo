906c89ff760b183f7e319730568f6e10c3a6f32b
Reginaldo-Dev <137166839+Reginaldo-Dev@users.noreply.github.com>
2024-08-07 17:50:25 -0300

    Configurando o typescript

--- teste.js
+++
@@ -1,4 +0,0 @@
-let nome = "Reginaldo";
-let idade = 44;
-
-console.log(`Oi, meu nome é ${nome} e eu tenho ${idade}`)
---
+++ teste.ts
@@ -0,0 +1,4 @@
+let nome: string = "Reginaldo";
+let idade: number = 44;
+
+console.log(`Oi, meu nome é ${nome} e eu tenho ${idade}`)
